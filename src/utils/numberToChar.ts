a187cd1b74d28b96995b61595990b5b155cad746
Abdu Tawfik <contact@abdu.dev>
2021-02-09 01:41:57 +0100

    Changed title and added few comments

--- src/utils/numberToChar.ts
+++ src/utils/numberToChar.ts
@@ -3,6 +3,9 @@ export const numberToChar = (num: number): string => {
   const reminder = Math.floor(num % 26);
 
   const char = String.fromCharCode(reminder + 97).toUpperCase();
-
+  // The idea of subtracting 1 is because the next numbers doesn't have zero in their system
+  // But our xlsheet title alphabet mapping have zero (A=0)
+  // So we need to subtract 1 to adjust this
+  // In case of using base 10, number after 9 wouldn't be 10 but rather 00 :D
   return division - 1 >= 0 ? numberToChar(division - 1) + char : char;
 };
